aa0bae07e441eb1d0438b945b80408977f8f2f44
Le Sang <sang.le@mservice.com.vn>
2022-04-22 19:07:54 +0700

    implement extended sequence protocol

--- sorted_frozen_set.py
+++ sorted_frozen_set.py
@@ -1,13 +1,22 @@
-class SortedFrozenSet:
+from bisect import bisect_left
+from collections.abc import Sequence
+from itertools import chain
+
+
+class SortedFrozenSet(Sequence):
 
     def __init__(self, items=None):
-        self._items = sorted(
+        self._items = tuple(sorted(
             set(items) if (items is not None)
             else list()
-        )
+        ))
 
     def __contains__(self, item):
-        return item in self._items
+        try:
+            self.index(item)
+            return True
+        except ValueError:
+            return False
 
     def __len__(self):
         return len(self._items)
@@ -26,16 +35,43 @@ class SortedFrozenSet:
         return "{type}({arg})".format(
             type=type(self).__name__,
             arg=(
-                repr(self._items)
+                "[{}]".format(
+                    ", ".join(
+                        map(repr, self._items)
+                    )
+                )
                 if self._items else ""
             )
-            # arg=(
-            #     repr(self._items)
-            #     if self._items else ""
-            # )
         )
 
     def __eq__(self, rhs):
         if not isinstance(rhs, type(self)):
             return NotImplemented
         return self._items == rhs._items
+
+    def __hash__(self):
+        return hash(
+            (type(self), self._items)
+        )
+
+    def __add__(self, rhs):
+        if not isinstance(rhs, type(self)):
+            return NotImplemented
+        return SortedFrozenSet(
+            chain(self._items, rhs._items)
+        )
+
+    def __mul__(self, rhs):
+        return self if rhs > 0 else SortedFrozenSet()
+
+    def __rmul__(self, lhs):
+        return self if lhs > 0 else SortedFrozenSet()
+
+    def count(self, item):
+        return int(item in self)
+
+    def index(self, item):
+        index = bisect_left(self._items, item)
+        if (index != len(self._items)) and self._items[index] == item:
+            return index
+        raise ValueError(f"{item!r} not found")
--- test_sorted_frozen_set.py
+++ test_sorted_frozen_set.py
@@ -128,6 +128,82 @@ class TestSequenceProtocol(unittest.TestCase):
     def test_slice_full(self):
         self.assertEqual(self.s[:], self.s)
 
+    def test_reversed(self):
+        s = SortedFrozenSet([1, 3, 5, 7])
+        r = reversed(s)
+        self.assertEqual(next(r), 7)
+        self.assertEqual(next(r), 5)
+        self.assertEqual(next(r), 3)
+        self.assertEqual(next(r), 1)
+        self.assertRaises(StopIteration, lambda: next(r))
+
+    def test_index_positive(self):
+        s = SortedFrozenSet([1, 5, 8, 9])
+        self.assertEqual(s.index(8), 2)
+
+    def test_index_positive(self):
+        s = SortedFrozenSet([1, 5, 8, 9])
+        with self.assertRaises(ValueError):
+            s.index(15)
+
+    def test_count_zero(self):
+        s = SortedFrozenSet([1, 5, 7, 9])
+        self.assertEqual(s.count(11), 0)
+
+    def test_count_one(self):
+        s = SortedFrozenSet([1, 5, 7, 9])
+        self.assertEqual(s.count(7), 1)
+
+    def test_add_disjoint(self):
+        s = SortedFrozenSet([1, 2, 3])
+        t = SortedFrozenSet([4, 5, 6])
+        self.assertEqual(s + t, SortedFrozenSet([1, 2, 3, 4, 5, 6]))
+
+    def test_add_equal(self):
+        s = SortedFrozenSet([1, 2, 3])
+        self.assertEqual(s + s, s)
+
+    def test_add_intersecting(self):
+        s = SortedFrozenSet([1, 2, 3])
+        t = SortedFrozenSet([3, 4, 5])
+        self.assertEqual(s + t, SortedFrozenSet([1, 2, 3, 4, 5]))
+
+    def test_add_type_error_left(self):
+        s = SortedFrozenSet([1, 2, 3])
+        t = (3, 4, 5)
+        with self.assertRaises(TypeError):
+            _ = s + t
+
+    def test_add_type_error_left(self):
+        t = SortedFrozenSet([1, 2, 3])
+        s = (3, 4, 5)
+        with self.assertRaises(TypeError):
+            _ = s + t
+
+    def test_repetition_zero_right(self):
+        s = SortedFrozenSet([4, 5, 6])
+        self.assertEqual(s * 0, SortedFrozenSet())
+
+    def test_repetition_negative_right(self):
+        s = SortedFrozenSet([4, 5, 6])
+        self.assertEqual(s * -1, SortedFrozenSet())
+
+    def test_repetition_nonzero_right(self):
+        s = SortedFrozenSet([4, 5, 6])
+        self.assertEqual(s * 100, s)
+
+    def test_repetition_zero_left(self):
+        s = SortedFrozenSet([4, 5, 6])
+        self.assertEqual(0 * s, SortedFrozenSet())
+
+    def test_repetition_negative_left(self):
+        s = SortedFrozenSet([4, 5, 6])
+        self.assertEqual(-1 * s, SortedFrozenSet())
+
+    def test_repetition_nonzero_left(self):
+        s = SortedFrozenSet([4, 5, 6])
+        self.assertEqual(100 * s, s)
+
 
 class TestReprProtocol(unittest.TestCase):
 
@@ -154,12 +230,21 @@ class TestEqualityProtocol(unittest.TestCase):
 
     def test_type_mismatch(self):
         self.assertFalse(
-            SortedFrozenSet([4, 5, 6])  == [4, 5, 6]
+            SortedFrozenSet([4, 5, 6]) == [4, 5, 6]
         )
 
     def test_identical(self):
         s = SortedFrozenSet([10, 11, 12])
         self.assertTrue(s == s)
+
+
+class TestHashableProtocol(unittest.TestCase):
+
+    def test_equal_sets_have_the_same_hash_code(self):
+        self.assertEqual(
+            hash(SortedFrozenSet([5, 2, 1, 4])),
+            hash(SortedFrozenSet([5, 2, 1, 4]))
+        )
 
 
 if __name__ == "__main__":
